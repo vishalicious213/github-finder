c79bcaf80eb569ea5f40a4ac8181c4870d41cb02
vishalicious213 <vishalicious213@gmail.com>
2020-02-17 00:46:05 -0500

    converted Navbar from class component to functional component with arrow function. Destructured Navbar and UserItem props.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,25 +1,23 @@
 import React from 'react'
 
-const UserItem = props => {
-        const { login, avatar_url, html_url } = props.user;
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+    return (
+        <div className='card text-center'>
+            <img
+                src={avatar_url}
+                alt=''
+                className='round-img'
+                style={{ width: '60px'}}
+            />
+            <h3>{login}</h3>
 
-        return (
-            <div className='card text-center'>
-                <img
-                    src={avatar_url}
-                    alt=''
-                    className='round-img'
-                    style={{ width: '60px'}}
-                />
-                <h3>{login}</h3>
-
-                <div>
-                    <a href={html_url} className='btn btn-dark btn-sm my-1'>
-                        More
-                    </a>
-                </div>
+            <div>
+                <a href={html_url} className='btn btn-dark btn-sm my-1'>
+                    More
+                </a>
             </div>
-        );
+        </div>
+    );
 }
 
 export default UserItem
