43f37914d6732b01df2b6ae4932a5b997dd586cd
vishalicious213 <vishalicious213@gmail.com>
2020-03-03 03:12:38 -0500

    passed user data state as props from app.js to users.js. removed static state and rendered props.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
   async componentDidMount() {
+    this.setState({ loading: true });
+
     const res = await axios
       .get('https://api.github.com/users');
 
-      console.log(res.data);
+      this.setState({ users: res.data, loading: false });
   }
 
   render() {
@@ -17,7 +24,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
