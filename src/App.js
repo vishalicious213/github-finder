792b1a87e76822caed449bed34dee51b06b0b4c9
vishalicious213 <vishalicious213@gmail.com>
2020-03-03 03:02:06 -0500

    getting user data from github with axios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  async componentDidMount() {
+    const res = await axios
+      .get('https://api.github.com/users');
+
+      console.log(res.data);
+  }
+
   render() {
     return (
       <div className="App">
